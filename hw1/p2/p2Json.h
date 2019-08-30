28b56a6903bbb53f582461b310419f66a204ac32
returnzhang0925 <53501651+returnzhang0925@users.noreply.github.com>
2019-08-30 19:30:12 +0800

    Add files via upload

--- hw1/p2/p2Json.h
+++ hw1/p2/p2Json.h
@@ -16,27 +16,40 @@ using namespace std;
 
 class JsonElem
 {
-public:
-   // TODO: define constructor & member functions on your own
-   JsonElem() {}
-   JsonElem(const string& k, int v): _key(k), _value(v) {}
+	public:
+		// TODO: define constructor & member functions on your own
+		JsonElem() {}
+		JsonElem(const string& k, int v): _key(k), _value(v) {}
 
-   friend ostream& operator << (ostream&, const JsonElem&);
+		void set_key(string);
+		void set_value(int);
+		string get_key();
+		int get_value();
 
-private:
-   string  _key;   // DO NOT change this definition. Use it to store key.
-   int     _value; // DO NOT change this definition. Use it to store value.
+		friend ostream& operator << (ostream&, const JsonElem&);
+
+	private:
+		string  _key;   // DO NOT change this definition. Use it to store key.
+		int     _value; // DO NOT change this definition. Use it to store value.
 };
 
 class Json
 {
-public:
-   // TODO: define constructor & member functions on your own
-   bool read(const string&);
+	public:
+		// TODO: define constructor & member functions on your own
+		bool read(const string&);
 
-private:
-   vector<JsonElem>       _obj;  // DO NOT change this definition.
-                                 // Use it to store JSON elements.
+		void print();
+		void sum();
+		void ave();
+		void max();
+		void min();
+		void add(string,int);
+		
+
+	private:
+		vector<JsonElem>       _obj;  // DO NOT change this definition.
+									// Use it to store JSON elements.
 };
 
 #endif // P2_TABLE_H
